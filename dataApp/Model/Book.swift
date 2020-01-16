17612c54482afd17badd2806534aa87b36898183
Kamaljit1504 <kaurkamaljit963@gmail.com>
2020-01-16 18:52:45 -0500

    Data app way create and write txt dat file
    // reference to project

---
+++ dataApp/Model/Book.swift
@@ -0,0 +1,23 @@
+//
+//  Book.swift
+//  dataApp
+//
+//  Created by MacStudent on 2020-01-16.
+//  Copyright © 2020 MacStudent. All rights reserved.
+//
+
+import Foundation
+class Book
+{
+    internal init(title: String, author: String, pages: Int, year: Int) {
+        self.title = title
+        self.author = author
+        self.pages = pages
+        self.year = year
+    }
+    
+    var title: String
+    var author: String
+    var pages: Int
+    var year: Int
+}
--- dataApp/ViewController.swift
+++ dataApp/ViewController.swift
@@ -10,11 +10,104 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+    var books: [Book]?
+    @IBOutlet var textFields: [UITextField]!
+    
+
+
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        loadData()
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(saveData), name: UIApplication.willResignActiveNotification, object: nil)
+    }
+    
+    func getFilePath() -> String
+    {
+        let documentPath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)
+        if documentPath.count > 0
+        {
+            let documentDirectory = documentPath[0]
+            let filePath = documentDirectory.appending("/data.txt" )
+            return filePath
+        }
+        return ""
+    }
+    func loadData()
+    {
+        let filePath = getFilePath()
+        books = [Book]()
+        
+        if FileManager.default.fileExists(atPath: filePath)
+        {
+            do
+            {
+                //extract Data
+                let fileContents = try String(contentsOfFile: filePath)
+                let contentArray = fileContents.components(separatedBy: "\n")
+                for content in contentArray
+                {
+                    let bookContent = content.components(separatedBy: ",")
+                    if bookContent.count == 4
+                    {
+                        let book = Book(title: bookContent[0], author: bookContent[1], pages: Int(bookContent[2])!, year: Int(bookContent[3])!)
+                        books?.append(book)
+                    }
+                }
+            }
+            catch
+            {
+                print(error)
+            }
+        }
+    }
+    @IBAction func addBook(_ sender: UIBarButtonItem)
+    {
+       let title = textFields[0].text ?? ""
+        let author = textFields[1].text ?? ""
+        let pages = Int(textFields[2].text ?? "") ?? 0
+        let year =  Int(textFields[3].text ?? "2020") ?? 2020
+        
+        let book = Book(title: title,author: author, pages: pages, year: year)
+        books?.append(book)
+        
+        for textField in textFields
+        {
+            textField.text = ""
+            textField.resignFirstResponder()
+            
+        }
+        
     }
 
+    override func prepare(for segue: UIStoryboardSegue, sender Sender: Any?)
+    {
+        if let BookTable = segue.destination as? BookTableVC
+        {
+            BookTable.books = self.books
+        }
+        
+    }
+    
+    @objc func saveData()
+    {
+        let filePath = getFilePath()
+        var saveString = ""
+        for book in books! {
+            saveString = "\(saveString)\(book.title),\(book.author),\(book.pages),\(book.author)\n"
+            
+        }
+        
+        do
+        {
+            try saveString.write(toFile: filePath, atomically: true, encoding: .utf8)
+        }
+        catch{
+            print(error)
+        }
+    }
 
 }
 
